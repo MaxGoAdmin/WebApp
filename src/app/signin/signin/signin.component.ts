d116f85f2a89ece2c420f9d353a72c7ff203c6fd
atif-maxgo123 <atif.maxgo@gmail.com>
2023-11-24 21:23:23 +0500

    changes

---
+++ src/app/signin/signin/signin.component.ts
@@ -0,0 +1,41 @@
+import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+import {
+  FormBuilder,
+  FormControl,
+  FormGroup,
+  Validators,
+} from '@angular/forms';
+
+@Component({
+  selector: 'app-signin',
+  templateUrl: './signin.component.html',
+  styleUrls: ['./signin.component.css'],
+})
+export class SigninComponent {
+  constructor(private fb: FormBuilder, private router: Router) {}
+
+  signinForm = this.fb.group({
+    email: ['', [Validators.required, Validators.email]],
+    password: ['', [Validators.required]],
+  });
+
+  public onSubmit(): void {
+    let email = this.signinForm.controls['email'].value;
+    let password = this.signinForm.controls['password'].value;
+
+    alert('Email: ' + email + '\nPassword: ' + password);
+  }
+  get f() {
+    return this.signinForm.controls;
+  }
+  openSignUp() {
+    this.router.navigate(['/signup']);
+  }
+  redirectForget() {
+    this.router.navigate(['/forgot-password']);
+  }
+  redirectProperty() {
+    this.router.navigate(['/properties']);
+  }
+}
