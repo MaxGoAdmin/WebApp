ec82a73a2c34fc99693aefa6e21861aa2814ac09
atif-maxgo123 <atif.maxgo@gmail.com>
2023-11-24 21:32:02 +0500

    001-atif for sign in and 003-atif for contact page

--- src/app/signin/signin.module.ts
+++ src/app/signin/signin.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ReactiveFormsModule } from '@angular/forms';
 import { RouterModule, Routes } from '@angular/router';
+import { SigninRoutingModule } from './signin-routing.module';
 import { SigninComponent } from './signin/signin.component';
 
 const routes: Routes = [{ path: '', component: SigninComponent }];
