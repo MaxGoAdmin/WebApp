9750e244e69e45cf76327abd2230a5d8ee7d8197
atif-maxgo123 <atif.maxgo@gmail.com>
2023-11-26 21:46:20 +0500

    demo

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,28 +1,7 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [
-  {
-    path: 'signin',
-    loadChildren: () =>
-      import('./signin/signin.module').then((m) => m.SigninModule),
-  },
-  { path: '', redirectTo: '/signin', pathMatch: 'full' },
-  {
-    path: 'properties',
-    loadChildren: () =>
-      import('./landing-page/landing-page.module').then(
-        (la) => la.LandingPageModule
-      ),
-  },
-
-  {
-    path: 'dashboard',
-    loadChildren: () =>
-      import('./dashboard/dashboard.module').then((m) => m.DashboardModule),
-  },
-
-];
+const routes: Routes = [{ path: '', redirectTo: '/signin', pathMatch: 'full' }];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes, { useHash: true })],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,9 +1,7 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { LandingPageModule } from './landing-page/landing-page.module';
-import { SigninModule } from './signin/signin.module';
 import { AppRoutingModule } from './app-routing.module';
-import { ReactiveFormsModule } from '@angular/forms';
+import { ReactiveFormsModule, FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
@@ -14,8 +12,6 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
     AppRoutingModule,
     BrowserAnimationsModule,
 
-    LandingPageModule,
-    SigninModule,
     ReactiveFormsModule,
     FormsModule,
   ],
