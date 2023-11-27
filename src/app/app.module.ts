feddccb4f599f458f7d2b390ae32508b3e51bea7
atif-maxgo123 <atif.maxgo@gmail.com>
2023-11-27 21:13:29 +0500

    contact page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,6 +5,7 @@ import { ReactiveFormsModule, FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { LandingPageModule } from './landing-page/landing-page.module';
+import { MatInputModule } from '@angular/material/input';
 
 @NgModule({
   declarations: [AppComponent],
@@ -15,6 +16,7 @@ import { LandingPageModule } from './landing-page/landing-page.module';
     ReactiveFormsModule,
     FormsModule,
     LandingPageModule,
+    MatInputModule,
   ],
   providers: [],
   bootstrap: [AppComponent],
